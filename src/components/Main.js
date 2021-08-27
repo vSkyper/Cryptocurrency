6798f47b9ac36e1430b31e8e00570dfb044eee59
Mateusz Piwowarski <skyperpl@gmail.com>
2021-08-27 21:36:23 +0200

    #97

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import { useRef } from 'react';
 import { styled } from '@material-ui/core/styles';
 import { Container, Link, Backdrop, CircularProgress } from '@material-ui/core';
-import { DataGrid } from '@material-ui/data-grid';
+import { DataGrid } from '@mui/x-data-grid';
 import { AreaChart, ResponsiveContainer, Area, YAxis } from 'recharts';
 import { Link as RouterLink } from 'react-router-dom';
 import useFetch from '../useFetch';
