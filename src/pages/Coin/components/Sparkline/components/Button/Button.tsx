05999f7f84959f47c74b1baa5dae536916402c81
Mateusz Piwowarski <matpiw1@st.amu.edu.pl>
2023-05-06 13:52:45 +0200

    Repair

--- src/pages/Coin/components/Sparkline/components/Button/Button.tsx
+++ src/pages/Coin/components/Sparkline/components/Button/Button.tsx
@@ -7,12 +7,14 @@ interface Props {
   actualDays: string;
   days: string;
   daysFormatted: string;
+  mobileDisappear: boolean;
 }
 
-export default function ButtonComponent({ setDays, actualDays, days, daysFormatted }: Props) {
+export default function ButtonComponent({ setDays, actualDays, days, daysFormatted, mobileDisappear }: Props) {
   return (
     <Button
       color={actualDays === days ? 'primary' : 'inherit'}
+      sx={{ display: { xs: mobileDisappear ? 'none' : 'block', sm: 'block' } }}
       onClick={() => setDays(days)}
     >
       {daysFormatted}
