1bc236233b4c6fbef66f127bb4e3033873ef0c1d
Mateusz Piwowarski <matpiw1@st.amu.edu.pl>
2023-05-06 11:19:39 +0200

    Switch from Math.pow to  Intl.NumberFormat

--- src/pages/Home/components/Global/components/Description/Description.tsx
+++ src/pages/Home/components/Global/components/Description/Description.tsx
@@ -6,12 +6,11 @@ interface Props {
 };
 
 export default function Description({ globalData }: Props) {
-  const marketCapText: string = (globalData.data.total_market_cap.usd / Math.pow(10, 12)
-  ).toLocaleString('en-US', {
-    maximumFractionDigits: 2,
+  const marketCapText: string = Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-  });
+    notation: 'compact'
+  }).format(globalData.data.total_market_cap.usd);
 
   const marketCapPercentage: string = (
     globalData.data.market_cap_change_percentage_24h_usd / 100
@@ -21,13 +20,11 @@ export default function Description({ globalData }: Props) {
     style: 'percent',
   });
 
-  const totalVolumeText: string = (
-    globalData.data.total_volume.usd / Math.pow(10, 9)
-  ).toLocaleString('en-US', {
-    maximumFractionDigits: 0,
+  const totalVolumeText: string = Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-  });
+    notation: 'compact'
+  }).format(globalData.data.total_volume.usd);
 
   const marketCapPercentageBTC: string = (
     globalData.data.market_cap_percentage.btc / 100
@@ -51,8 +48,7 @@ export default function Description({ globalData }: Props) {
 
   return (
     <Typography fontWeight='fontWeightLight'>
-      The global cryptocurrency market cap today is {marketCapText}{' '}
-      Trillion, a{' '}
+      The global cryptocurrency market cap today is {marketCapText}, a{' '}
       <Typography
         fontWeight='fontWeightLight'
         component='span'
@@ -65,7 +61,7 @@ export default function Description({ globalData }: Props) {
         {marketCapPercentage}
       </Typography>{' '}
       change in the last 24 hours. Total cryptocurrency trading volume in
-      the last day is at {totalVolumeText} Billion. Bitcoin dominance is at{' '}
+      the last day is at {totalVolumeText}. Bitcoin dominance is at{' '}
       {marketCapPercentageBTC} and Ethereum dominance is at{' '}
       {marketCapPercentageETH}. CoinGecko API is now tracking{' '}
       {cryptocurrencies} cryptocurrencies.
