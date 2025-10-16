c0a1584c978590f5d6d28dfaf0aaf0e9dd91436c
Mateusz Piwowarski <skyperpl@gmail.com>
2025-10-16 23:46:32 +0200

    Remove StyledEngineProvider wrapper from App component

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { StyledEngineProvider } from '@mui/material/styles';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <App />
-    </StyledEngineProvider>
+    <App />
   </React.StrictMode>
 );
